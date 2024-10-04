f123811f08f6f954ac23fd95c4f214a54860e811
sharique <sharique.kalim.1994@gmail.com>
2024-10-04 23:25:53 +0530

    GitIgnore changes

--- NajmHouseAPI.Domian/obj/Debug/net6.0/NajmHouseAPI.Domian.AssemblyInfo.cs
+++ NajmHouseAPI.Domian/obj/Debug/net6.0/NajmHouseAPI.Domian.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NajmHouseAPI.Domian")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06de7b76167e5b178a229b66ca5e5967a83a0cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NajmHouseAPI.Domian")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NajmHouseAPI.Domian")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- NajmHouseAPI.Infrastructure.IoC/obj/Debug/net6.0/NajmHouseAPI.Infrastructure.IoC.AssemblyInfo.cs
+++ NajmHouseAPI.Infrastructure.IoC/obj/Debug/net6.0/NajmHouseAPI.Infrastructure.IoC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NajmHouseAPI.Infrastructure.IoC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06de7b76167e5b178a229b66ca5e5967a83a0cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NajmHouseAPI.Infrastructure.IoC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NajmHouseAPI.Infrastructure.IoC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
